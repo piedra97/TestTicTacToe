2bd7f9e3052bcffb946dd91a6348e188da698396
Pablo Nuñez <pablonura91@gmail.com>
2019-03-15 16:38:29 +0100

    Update Board.java

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -1,4 +1,31 @@
 package Model;
 
 public class Board {
+
+    private Square[][] squares;
+
+    public Board (Square[][] squares){
+        this.squares = squares;
+    }
+
+    public Square getSquare(Position position){
+        return squares[position.getX()][position.getY()];
+    }
+
+    public void setSquareSymbol(Position position, Symbol symbol){
+        squares[position.getX()][position.getY()].setSymbol(symbol);
+    }
+
+    public Boolean isFull(){
+        boolean isFull = true;
+
+        for (int x = 0; x < 3; x++){
+            for (int y = 0; y < 3; y++){
+                if (squares[x][y].getSymbol() == Symbol.E){
+                    isFull = false;
+                }
+            }
+        }
+        return isFull;
+    }
 }
